70bed34e57a810239c27097232a74fd47814fd57
Mendess2526 <pedro.mendes.26@gmail.com>
2018-12-24 14:56:57 +0000

    sockets

--- src/main.rs
+++ src/main.rs
@@ -3,42 +3,29 @@ mod auction_house;
 use crate::auction_house::{AuctionHouse, ClientError, item::ServerType};
 
 use std::io;
+use std::io::Read;
 use std::str::FromStr;
+use std::thread;
+use std::net::{TcpListener, TcpStream};
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut auction_house = AuctionHouse::new();
-    auction_house.add(ServerType::Slow);
-    auction_house.add(ServerType::Fast);
-    auction_house.add(ServerType::Slow);
-    auction_house.add(ServerType::Fast);
-    println!("{:#?}", auction_house);
-    let emails = ["pedro@email.com", "pedro@email.com"];
-    for email in emails.iter() {
-        match auction_house.register(email, "password") {
-            Err(e) => println!("{:?}", e),
-            _ => (),
-        }
-    }
-
-    // for _ in 0..3 {
-    //     match auction_house.buy(ServerType::Slow, emails[0]) {
-    //         Err(e) => println!("{:?}", e),
-    //         _ => (),
-    //     }
-    // };
-    // println!("{:#?}", auction_house);
-    // println!("{}", auction_house.login(emails[0], "password"));
-    // println!("{}", auction_house.login(emails[0], "wordpass"));
-    // println!("{}", auction_house.login("unregitered@email", "passfrase"));
-    use_loop(&mut auction_house);
+    for _ in 0..30 { auction_house.add(ServerType::Slow); }
+    for _ in 0..4 { auction_house.add(ServerType::Fast); }
+    let server = TcpListener::bind("127.0.0.1:12345")?;
+    let (stream, _addr) = server.accept()?;
+    let thread = thread::spawn(move || use_loop(&mut auction_house, &mut stream));
+    thread.join();
+    Ok(())
 }
 
-fn use_loop(ah :&mut AuctionHouse) {
-    let mut input = String::new();
+fn use_loop(ah :&mut AuctionHouse, stream :&mut TcpStream) {
     let mut user = None;
+    let mut socket_input = [u8;1024];
     loop {
         input.clear();
-        if let Err(_) = io::stdin().read_line(&mut input) { break };
+        if let Err(_) = stream.read_to_end(&mut socket_input) { break };
+        let mut input = String::from_utf8_lossy(socket_input);
         if input.trim() == "quit" { break }
         let command = input.split_whitespace().map(|s| s.trim()).collect::<Vec<&str>>();
         if command.len() == 0 { continue }
@@ -118,51 +105,51 @@ impl Command {
 
     fn ls(args :&[&str], ah :&AuctionHouse, user :&Option<String>)
         -> Result<Command, CommandError>
-    {
-        if args.len() == 0 {
-            let stock = ah.ls();
-            let mut result = String::from_str("Type\tAmount in stock\n=======================\n")
-                .unwrap();
-            for (k, v) in stock.iter() {
-                result += &format!("{:?}\t{}\n", k, v.len());
-            }
-            Ok(Command::Ls(result))
-        } else if args[0] == "-m" {
-            if user.is_none() {
-                Err(CommandError("You must be logged in to use this!".into()))
+        {
+            if args.len() == 0 {
+                let stock = ah.ls();
+                let mut result = String::from_str("Type\tAmount in stock\n=======================\n")
+                    .unwrap();
+                for (k, v) in stock.iter() {
+                    result += &format!("{:?}\t{}\n", k, v.len());
+                }
+                Ok(Command::Ls(result))
+            } else if args[0] == "-m" {
+                if user.is_none() {
+                    Err(CommandError("You must be logged in to use this!".into()))
+                } else {
+                    Ok(Command::Ls(format!("{:?}", ah.ls_m(user.as_ref().unwrap())
+                                           .iter()
+                                           .map(|d| d.server_type())
+                                           .collect::<Vec<ServerType>>())
+                                  ))
+                }
             } else {
-                Ok(Command::Ls(format!("{:?}", ah.ls_m(user.as_ref().unwrap())
-                                       .iter()
-                                       .map(|d| d.server_type())
-                                       .collect::<Vec<ServerType>>())
-                               ))
+                Err(CommandError("Usage: ls [-m]\n\t-m show my droplets".into()))
             }
-        } else {
-            Err(CommandError("Usage: ls [-m]\n\t-m show my droplets".into()))
         }
-    }
 
     fn buy(args :&[&str], ah :&mut AuctionHouse, user :&Option<String>)
-            -> Result<Command, CommandError>
-    {
-        if user.is_none() {
-            Err(CommandError("You must be logged in to use this!".into()))
-        } else if args.len() < 1 {
-            Err(CommandError("Usage: buy <Fast,Slow>".into()))
-        } else {
-            let maybe_st = ServerType::from_str(args[0]);
-            if maybe_st.is_none() {
-                return Err(CommandError("Invalid server type!".into()));
-            }
-            let st = maybe_st.unwrap();
-            use crate::auction_house::AuctionError::*;
-            match ah.buy(st, &user.as_ref().unwrap()) {
-                Err(InvalidClient(_)) => unreachable!(),
-                Err(OutOfStock(_)) => Err(CommandError("Out of stock".into())),
-                Err(NotEnughFunds(p,f)) =>
-                    Err(CommandError(format!("not enough funds: Price {}, Funds: {}", p, f))),
-                Ok(()) => Ok(Command::Buy),
+        -> Result<Command, CommandError>
+        {
+            if user.is_none() {
+                Err(CommandError("You must be logged in to use this!".into()))
+            } else if args.len() < 1 {
+                Err(CommandError("Usage: buy <Fast,Slow>".into()))
+            } else {
+                let maybe_st = ServerType::from_str(args[0]);
+                if maybe_st.is_none() {
+                    return Err(CommandError("Invalid server type!".into()));
+                }
+                let st = maybe_st.unwrap();
+                use crate::auction_house::AuctionError::*;
+                match ah.buy(st, &user.as_ref().unwrap()) {
+                    Err(InvalidClient(_)) => unreachable!(),
+                    Err(OutOfStock(_)) => Err(CommandError("Out of stock".into())),
+                    Err(NotEnughFunds(p,f)) =>
+                        Err(CommandError(format!("not enough funds: Price {}, Funds: {}", p, f))),
+                    Ok(()) => Ok(Command::Buy),
+                }
             }
         }
-    }
 }
